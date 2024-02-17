1615cacef79c5d4dddbdf236fae6fd35df28d9b7
Jacob <97726908+arnez-25@users.noreply.github.com>
2024-02-17 15:17:36 -0800

    Added post request for each month
    Now each month makes the same post request when clicked on the front end. Also added ids for the orders in the html for the modifications in the post request.

--- hw3/public/javascripts/hw3_script.js
+++ hw3/public/javascripts/hw3_script.js
@@ -29,64 +29,134 @@ $(document).ready(function() {
 
     $("#jan").click(function(){
         $("#hoverbtn").text('January');
+        // We have a post in orders that sends the json so when we call post here we can finally obtain it
         $.post('/orders', function(data){
-            alert(data[0].topping)
+            //alert(data[0].topping + ", " + data[0].quantity )
+            $("#order1").text(data[0].quantity + " " + data[0].topping);
+            $("#order2").text(data[1].quantity + " " + data[1].topping);
+            $("#order3").text(data[2].quantity + " " + data[2].topping);
         });
 
     });
 
     $("#feb").click(function(){
         $("#hoverbtn").text('Feburary');
+        $.post('/orders', function(data){
+            //alert(data[0].topping + ", " + data[0].quantity )
+            $("#order1").text(data[0].quantity + " " + data[0].topping);
+            $("#order2").text(data[1].quantity + " " + data[1].topping);
+            $("#order3").text(data[2].quantity + " " + data[2].topping);
+        });
 
     });
     
     $("#mar").click(function(){
         $("#hoverbtn").text('March');
+        $.post('/orders', function(data){
+            //alert(data[0].topping + ", " + data[0].quantity )
+            $("#order1").text(data[0].quantity + " " + data[0].topping);
+            $("#order2").text(data[1].quantity + " " + data[1].topping);
+            $("#order3").text(data[2].quantity + " " + data[2].topping);
+        });
 
     });
     
     $("#apr").click(function(){
         $("#hoverbtn").text('April');
+        $.post('/orders', function(data){
+            //alert(data[0].topping + ", " + data[0].quantity )
+            $("#order1").text(data[0].quantity + " " + data[0].topping);
+            $("#order2").text(data[1].quantity + " " + data[1].topping);
+            $("#order3").text(data[2].quantity + " " + data[2].topping);
+        });
 
     });
 
     $("#may").click(function(){
         $("#hoverbtn").text('May');
+        $.post('/orders', function(data){
+            //alert(data[0].topping + ", " + data[0].quantity )
+            $("#order1").text(data[0].quantity + " " + data[0].topping);
+            $("#order2").text(data[1].quantity + " " + data[1].topping);
+            $("#order3").text(data[2].quantity + " " + data[2].topping);
+        });
 
     });
 
     $("#jun").click(function(){
         $("#hoverbtn").text('June');
+        $.post('/orders', function(data){
+            //alert(data[0].topping + ", " + data[0].quantity )
+            $("#order1").text(data[0].quantity + " " + data[0].topping);
+            $("#order2").text(data[1].quantity + " " + data[1].topping);
+            $("#order3").text(data[2].quantity + " " + data[2].topping);
+        });
 
     });
 
     $("#jul").click(function(){
         $("#hoverbtn").text('July');
+        $.post('/orders', function(data){
+            //alert(data[0].topping + ", " + data[0].quantity )
+            $("#order1").text(data[0].quantity + " " + data[0].topping);
+            $("#order2").text(data[1].quantity + " " + data[1].topping);
+            $("#order3").text(data[2].quantity + " " + data[2].topping);
+        });
 
     });
 
     $("#aug").click(function(){
         $("#hoverbtn").text('August');
+        $.post('/orders', function(data){
+            //alert(data[0].topping + ", " + data[0].quantity )
+            $("#order1").text(data[0].quantity + " " + data[0].topping);
+            $("#order2").text(data[1].quantity + " " + data[1].topping);
+            $("#order3").text(data[2].quantity + " " + data[2].topping);
+        });
 
     });
 
     $("#sep").click(function(){
         $("#hoverbtn").text('September');
+        $.post('/orders', function(data){
+            //alert(data[0].topping + ", " + data[0].quantity )
+            $("#order1").text(data[0].quantity + " " + data[0].topping);
+            $("#order2").text(data[1].quantity + " " + data[1].topping);
+            $("#order3").text(data[2].quantity + " " + data[2].topping);
+        });
 
     });
 
     $("#oct").click(function(){
         $("#hoverbtn").text('October');
+        $.post('/orders', function(data){
+            //alert(data[0].topping + ", " + data[0].quantity )
+            $("#order1").text(data[0].quantity + " " + data[0].topping);
+            $("#order2").text(data[1].quantity + " " + data[1].topping);
+            $("#order3").text(data[2].quantity + " " + data[2].topping);
+        });
 
     });
 
     $("#nov").click(function(){
         $("#hoverbtn").text('November');
+        $.post('/orders', function(data){
+            //alert(data[0].topping + ", " + data[0].quantity )
+            $("#order1").text(data[0].quantity + " " + data[0].topping);
+            $("#order2").text(data[1].quantity + " " + data[1].topping);
+            $("#order3").text(data[2].quantity + " " + data[2].topping);
+        });
 
     });
 
     $("#dec").click(function(){
         $("#hoverbtn").text('December');
+        $.post('/orders', function(data){
+            //alert(data[0].topping + ", " + data[0].quantity )
+            $("#order1").text(data[0].quantity + " " + data[0].topping);
+            $("#order2").text(data[1].quantity + " " + data[1].topping);
+            $("#order3").text(data[2].quantity + " " + data[2].topping);
+        });
 
     });
 
--- hw3/routes/orders.js
+++ hw3/routes/orders.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
   res.json(cheesecakes);
 });
 
-
+/* POST orders when a post request is sent on the client side it sends the json object */
 router.post('/', function(req, res) {
   res.send(cheesecakes);
 });
